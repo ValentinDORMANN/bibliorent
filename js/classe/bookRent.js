67d8a81d737942732dbeb21e98c652e1428c1185
ValentinDORMANN <valentin.dormann@gmail.com>
2017-03-25 18:09:06 +0100

    refactor constructor, add setter

--- js/classe/bookRent.js
+++ js/classe/bookRent.js
@@ -5,17 +5,57 @@
  * @param {Book[?]}  books    [description]
  * @param {Date}     rentDate [description]
  */
-let BookRent = function(consumer, books, rentDate){
-	this.isPaid = false;																									// boolean
-	this.books = books;																										// Book[?]
-	this.retrivalDate = this.addDays(rentDate, BookRent.MAX_RENTAL_DAY);	// Date
-	this.rentDate = rentDate;																							// Date
-	this.consumer = consumer;																							// Consumer
+let BookRent = function(){
+	this._id;								// string
+	this.isPaid = false;	  // boolean
+	this.books = [];				// Book[?]
+	this.retrivalDate;	    // Date
+	this.rentDate;					// Date
+	this.consumer;					// Consumer
+	switch(arguments.length){
+		case 3: // BookRent = function(consumer, books, rentDate)
+			this.consumer = arguments[0];
+			this.books = arguments[1];
+			this.rentDate = arguments[2];
+			this.retrivalDate = this.addDays(this.rentDate, BookRent.MAX_RENTAL_DAY);
+			break;
+	}
 };
 /**
  * @constant  {String}
  */
 BookRent.MAX_RENTAL_DAY = 14;
+BookRent.prototype._id = function(id){
+	this._id = id;
+	return this;
+};
+BookRent.prototype.isPay = function(isPaidStr){
+	this.isPaid = isPaidStr;
+	return this;
+};
+BookRent.prototype.book = function(book){
+	if(Array.isArray(book)){ 	// case Book[?]
+		for(let i in book){
+			this.books.push(book)
+		}
+	}else{										// case Book
+		this.books.push(book);
+	}
+	return this;
+};
+BookRent.prototype.retrivalDateIso = function(retrivalDateIso){
+	this.retrivalDate = new Date(retrivalDateIso);
+	return this;
+};
+BookRent.prototype.rentDateIso = function(rentDateIso){
+	this.rentDate = new Date(rentDateIso);
+	return this;
+};
+BookRent.prototype.consumer = function(consumer){
+	this.consumer = consumer;
+	return this;
+};
+
 /**
  * Add days to a date
  * @function addDays
