d33a66f4712f8e5ec5213d0bc0a3a12b5a9f25ef
ValentinDORMANN <valentin.dormann@gmail.com>
2017-03-25 18:11:53 +0100

    add jsdoc

--- js/classe/library.js
+++ js/classe/library.js
@@ -1,7 +1,17 @@
+/**
+ * Represenres a library
+ * @construct
+ */
 let Library = function(){
 	this.bookRepository;	    // BookRepository
 	this.bookRentRepository;    // BookRentRepository
 };
+/**
+ * A rent was made by consumer
+ * @param  {Consumer} consumer [description]
+ * @param  {Book[?]}  books    [description]
+ * @return {void}              [description]
+ */
 Library.prototype.rent = function(consumer, books){
 	this.bookRepository.remove(books);
 	this.bookRentRepository.add(new BookRent(consumer, books, new Date()));
